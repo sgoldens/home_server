a451fc7b9d5f0f9fee510cbc85772aa8e18f21cf
Sasha Goldenson <sgoldens@gmail.com>
2017-03-18 09:40:18 -0700

    Add passport and login strategies

--- services/passport.js
+++ services/passport.js
@@ -1,5 +1,33 @@
+// Incoming requests hit passport for strategies 1 or 2
+// #1 - Verify user with a JWT
+// #2 - Verify user with a username and password
+// Then hand off to the Route Handler
+
 const passport = require('passport');
 const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// Setup options for JWT Strategy
+const jwtOptions = {};
+
+// Create JWT strategy
+// payload is the decoded JWT token
+// done is a callback to... 
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // See if the user ID in the payload exists in our database
+  // If it does, call 'done' with that
+  // otherwise, call done without a user object
+  User.findById(payload.sub, function(err, user) {
+    if (err) { return done(err, false); }
+
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+});
+// Tell passport to use this strategy
+
